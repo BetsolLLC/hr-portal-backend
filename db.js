80ee28d00797a448b196a8dd1262dc3eeeb7b5f7
Annoel Skaria <annoel.skaria@btech.christuniversity.in>
2022-06-23 17:48:43 +0530

    database added

--- db.js
+++ db.js
@@ -3,12 +3,13 @@ import { createRequire } from "module";
 const require = createRequire(import.meta.url);
 //connecting to the database using Pool
 const Pool = require ("pg").Pool;
+require('dotenv').config()
 const pool = new Pool({
-    user : "postgres",
-    password : "root",
-    host:"localhost",
-    port:5433,
-    database:"userpool"
+    host:process.env.db_host,
+    port:process.env.db_port,
+    user:process.env.db_user,
+    password:process.env.db_password,
+    database: process.env.db_database
 })
 
 // module.exports = pool;
--- server.js
+++ server.js
@@ -8,5 +8,5 @@ const app = express()
 app.use(express.json());
 app.use('/api/auth', authRoutes);
 
-const PORT = process.env.PORT || 3000;
+ const PORT = process.env.PORT || 3000;
 app.listen(PORT, console.log(`Server listening on port ${PORT}`));
