f2c9d1b15a42e50969ee66d1cfc5a6e3a6e9623d
bhanuprakash-betsol <Bhanu.Prakash@betsol.com>
2022-06-24 09:56:01 +0530

    adding password and encryption

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,4 +1,7 @@
 import pool from '../db.js';
+import bcrypt from 'bcrypt';;
+import generator from 'generate-password';
+
 const login = async (req, res) => {
 return	res.send("User logged in");
 };
@@ -6,19 +9,29 @@ return	res.send("User logged in");
 const adduser = async(req,res) => {
     try {
         //checking the user already exist 
-        const { name,email} = req.body;
+        const { name,email,batch,number} = req.body;
 
-        const user = await pool.query("SELECT * FROM users WHERE user_email = $1", [
+        const user = await pool.query("SELECT * FROM users WHERE email = $1", [
             email
           ]);
       
         if (user.rows.length > 0) {
-            return res.status(401).json("User already exist!");
+            return res.status(409).json("User already exist!");
         }
         
+        //generating a random password
+        var password = generator.generate({
+            length: 10,
+            numbers: true
+        });
+
+        //encrption of the password 
+
+        const salt = await bcrypt.genSalt(10);
+        const bcryptPassword = await bcrypt.hash(password, salt);
         // adding new user
-        const newUser = await pool.query("INSERT INTO users (user_name,user_email,user_password) VALUES ($1,$2,'welcome@123')",
-        [name,email]
+        const newUser = await pool.query("INSERT INTO users (name,email,batch,phone_number,password) VALUES ($1,$2,$3,$4,$5)",
+        [name,email,batch,number,bcryptPassword]
         );
         res.send("user added");
     
