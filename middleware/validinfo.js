0acd0489e70ef66970ef78af24dc3ca5959b033d
bhanuprakash-betsol <Bhanu.Prakash@betsol.com>
2022-07-05 19:31:12 +0530

    created a route for updating

--- middleware/validinfo.js
+++ middleware/validinfo.js
@@ -1,5 +1,5 @@
 import { errorResponse } from "../interceptor/error.js";
-
+import { FILE_SIZE } from "../config/config.js";
 const validEmail = (email) => {
   return /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email);
 };
@@ -10,6 +10,15 @@ const validinfo = function (req, res, next) {
   if (req.path === "/addusers" || req.path === "/login") {
     if (!validEmail(email)) {
       return errorResponse(res, 400, "invalid email");
+    }
+  }
+
+  if (req.path === "/upload") {
+    if (!req.file || req.file.mimetype !== "application/pdf") {
+      return errorResponse(res, 400, "File missing or invalid");
+    }
+    if (!req.file || req.file.size > FILE_SIZE) {
+      return errorResponse(res, 400, "File size is greater than 1MB");
     }
   }
 
