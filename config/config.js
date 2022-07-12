6cc5906dc197e0e028eac3c0d953876f5fa9e445
pramod.janardhana <pramod.janardhan@betsol.com>
2022-07-12 16:41:29 +0530

    not sending admins in get users api

--- config/config.js
+++ config/config.js
@@ -1,53 +1,71 @@
 import "dotenv/config";
 
-const loadEnv = (key) => {
-  if (key === undefined || key === "" || key.trim() === "") {
-    console.error("Missing env variable(s)");
+const loadEnv = (key, value) => {
+  if (value === undefined || value === "" || value.trim() === "") {
+    console.error("Missing env variable:", key);
     process.exit(1);
   }
 
-  return key.trim();
+  return value.trim();
 };
 
 // Server Config
 const SALT = 10;
-const ENV = loadEnv(process.env.ENV);
-const PORT = loadEnv(process.env.PORT);
-const LOG_LEVEL = loadEnv(process.env.LOG_LEVEL);
+const ENV = loadEnv("ENV", process.env.ENV);
+const PORT = loadEnv("PORT", process.env.PORT);
+const LOG_LEVEL = loadEnv("LOG_LEVEL", process.env.LOG_LEVEL);
 
 // Roles
 const ROLES = { END_USER: "user", ADMIN: "admin" };
 
 // Database Config
-const DB_HOST = loadEnv(process.env.DB_HOST);
-const DB_PORT = loadEnv(process.env.DB_PORT);
-const DB_USER = loadEnv(process.env.DB_USER);
-const DB_PASSWORD = loadEnv(process.env.DB_PASSWORD);
-const DB_DATABASE = loadEnv(process.env.DB_DATABASE);
+const DB_HOST = loadEnv("DB_HOST", process.env.DB_HOST);
+const DB_PORT = loadEnv("DB_PORT", process.env.DB_PORT);
+const DB_USER = loadEnv("DB_USER", process.env.DB_USER);
+const DB_PASSWORD = loadEnv("DB_PASSWORD", process.env.DB_PASSWORD);
+const DB_DATABASE = loadEnv("DB_DATABASE", process.env.DB_DATABASE);
 
 // JWT Config
-const JWTSECRET = loadEnv(process.env.JWTSECRET);
-const TOKEN_EXPIRY = loadEnv(process.env.TOKEN_EXPIRY);
+const JWTSECRET = loadEnv("JWTSECRET", process.env.JWTSECRET);
+const TOKEN_EXPIRY = loadEnv("TOKEN_EXPIRY", process.env.TOKEN_EXPIRY);
 
 // AWS S3 Config
-const AWS_ACCESS_KEY_ID = loadEnv(process.env.AWS_ACCESS_KEY_ID);
-const AWS_SECRET_ACCESS_KEY = loadEnv(process.env.AWS_SECRET_ACCESS_KEY);
-const AWS_BUCKET = loadEnv(process.env.AWS_BUCKET);
-const AWS_REGION = loadEnv(process.env.AWS_REGION);
-const FROM_EMAIL = loadEnv(process.env.FROM_EMAIL);
-const FORGOT_PASSWORD = loadEnv(process.env.FORGOT_PASSWORD);
+const AWS_ACCESS_KEY_ID = loadEnv(
+  "AWS_ACCESS_KEY_ID",
+  process.env.AWS_ACCESS_KEY_ID
+);
+const AWS_SECRET_ACCESS_KEY = loadEnv(
+  "AWS_SECRET_ACCESS_KEY",
+  process.env.AWS_SECRET_ACCESS_KEY
+);
+const AWS_BUCKET = loadEnv("AWS_BUCKET", process.env.AWS_BUCKET);
+const AWS_REGION = loadEnv("AWS_REGION", process.env.AWS_REGION);
+const FROM_EMAIL = loadEnv("FROM_EMAIL", process.env.FROM_EMAIL);
+const FORGOT_PASSWORD = loadEnv("FORGOT_PASSWORD", process.env.FORGOT_PASSWORD);
 
 // Upload doc configs
-const FILE_SIZE = loadEnv(process.env.FILE_SIZE);
-const UPLOAD_FILE_ALGORITHM = loadEnv(process.env.UPLOAD_FILE_ALGORITHM);
-const UPLOAD_FILE_SECRET = loadEnv(process.env.UPLOAD_FILE_SECRET);
+const FILE_SIZE = loadEnv("FILE_SIZE", process.env.FILE_SIZE);
+const UPLOAD_FILE_ALGORITHM = loadEnv(
+  "UPLOAD_FILE_ALGORITHM",
+  process.env.UPLOAD_FILE_ALGORITHM
+);
+const UPLOAD_FILE_SECRET = loadEnv(
+  "UPLOAD_FILE_SECRET",
+  process.env.UPLOAD_FILE_SECRET
+);
 const UPLOAD_FILE_SIGNATURE_VALIDITY_IN_SECONDS = 120;
 
 // Download files configs
-const MAX_FILE_DOWNLOAD_COUNT = loadEnv(process.env.MAX_FILE_DOWNLOAD_COUNT);
+const MAX_FILE_DOWNLOAD_COUNT = loadEnv(
+  "MAX_FILE_DOWNLOAD_COUNT",
+  process.env.MAX_FILE_DOWNLOAD_COUNT
+);
 
-//blacklisted password
-const Blacklist = loadEnv(process.env.Blacklist)
+//Blacklisted password
+const BLACKLISTED_PASSWORDS = loadEnv(
+  "BLACKLISTED_PASSWORDS",
+  process.env.BLACKLISTED_PASSWORDS
+);
 export {
   ENV,
   PORT,
@@ -71,9 +89,6 @@ export {
   AWS_REGION,
   FROM_EMAIL,
   MAX_FILE_DOWNLOAD_COUNT,
-
   FORGOT_PASSWORD,
-
-Blacklist,
-
+  BLACKLISTED_PASSWORDS,
 };
