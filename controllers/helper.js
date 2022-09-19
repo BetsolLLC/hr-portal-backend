283efc1a8e93d9e95e22c3f3fd2e401fe2b80443
pramod.janardhana <pramod.janardhan@betsol.com>
2022-09-19 17:40:27 +0530

    added api to process user addition from csv

--- controllers/helper.js
+++ controllers/helper.js
@@ -1,4 +1,78 @@
 import db from "../db.js";
+import generator from "generate-password";
+import { ROLES, SALT } from "../config/config.js";
+import { logger } from "../logger.js";
+import { mailer } from "../service/mailer.js";
+import bcrypt from "bcrypt";
+import moment from "moment";
+import jwtGenerator from "../utils/jwtGenerator.js";
+
+const addUserUtility = async (name, email, batch, phoneNumber) => {
+  if (name === "" || name === null) {
+    name = email;
+  }
+
+  try {
+    // checking the user already exist
+    {
+      const user = await db.query("SELECT * FROM users WHERE email = $1", [
+        email,
+      ]);
+
+      if (user.rows.length > 0) {
+        logger.debug(`user with email(${email}) is exists`);
+        return { statusCode: 409, errorMsg: "user already exist" };
+      }
+    }
+
+    // generating a random password
+    const password = generator.generate({
+      length: 10,
+      numbers: true,
+    });
+
+    // only for development purpose this needs to removed later
+    // console.log(password);
+
+    // hashing password
+    const salt = await bcrypt.genSalt(SALT);
+    const bcryptPassword = await bcrypt.hash(password, salt);
+
+    // adding user to DB
+    await db.query(
+      "INSERT INTO users (name,email,batch,phone_number,password) VALUES ($1,$2,$3,$4,$5)",
+      [name, email, batch, phoneNumber, bcryptPassword]
+    );
+
+    // using email as id in the token as id is not available
+    const token = jwtGenerator(email, name, batch, email, ROLES.END_USER);
+    logger.debug(`user(${email}) add successfully`);
+
+    const isMailSent = await mailer(name, email, token, password);
+    if (!isMailSent) {
+      logger.debug(`error sending  creadential mail to user: ${email}`);
+
+      try {
+        await db.query("DELETE FROM users WHERE email = $1", [email]);
+      } catch (err) {
+        logger.debug(`error deleting user: ${email} from DB`);
+        return { statusCode: 500, errorMsg: `error adding user` };
+      }
+
+      logger.debug(`deleted user${email} from DB`);
+      return { statusCode: 500, errorMsg: `error adding user` };
+    } else {
+      logger.debug(
+        `Credential mail sent to (${email}) successfully ${isMailSent}`
+      );
+    }
+
+    return { statusCode: 201, successMsg: "user added" };
+  } catch (err) {
+    logger.error(`error adding user: ${err}`);
+    return { statusCode: 500, errorMsg: "error adding user" };
+  }
+};
 
 const getFileUploadPath = async (doc_id, user) => {
   const details = await db.query(
@@ -18,4 +92,23 @@ const getFileNameFromPath = (path) => {
   return pathArray[pathArray.length - 1] + ".pdf";
 };
 
-export { getFileUploadPath, getFileNameFromPath };
+const isEmailValid = (email) => {
+  return /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email);
+};
+
+const isPhoneNumberValid = (phoneNumber) => {
+  return phoneNumber.length === 10;
+};
+
+const isDateValid = (date) => {
+  return moment(date, "MM/DD/YYYY", true).isValid();
+};
+
+export {
+  getFileUploadPath,
+  getFileNameFromPath,
+  addUserUtility,
+  isEmailValid,
+  isPhoneNumberValid,
+  isDateValid,
+};
--- middleware/fileMiddleware.js
+++ middleware/fileMiddleware.js
@@ -1,6 +1,10 @@
-import { FILE_SIZE } from "../config/config.js";
+import { FILE_SIZE, BULK_USER_ADDITION_FILE_SIZE } from "../config/config.js";
 import multer from "multer";
 const storage = multer.memoryStorage();
 const upload = multer({ storage, limits: { fileSize: FILE_SIZE } });
+const bulkUserAdditionMiddleware = multer({
+  storage,
+  limits: { fileSize: BULK_USER_ADDITION_FILE_SIZE },
+});
 
-export { upload };
+export { upload, bulkUserAdditionMiddleware };
